45d11972967c0d717910df59a62410279abfd37e
Toshiki <toshiki.fujii@outlook.com>
2024-06-29 15:55:18 +0900

    simple score app

--- app/main.py
+++ app/main.py
@@ -1,12 +1,49 @@
 from pathlib import Path
+import requests
+import pandas as pd
 
 import pandas as pd
 import streamlit as st
 
+from module import compare_positions, format_trans_table
+
+company_id = None
+trans_df = None
+
+mst_url = 'https://circuit-trial.stg.rd.ds.sansan.com/api/cards/?offset=0&limit=2000'
+mst_r = requests.get(mst_url)
+mst_df = pd.DataFrame(mst_r.json())
+
+company_dict = mst_df[["company_id", "company_name"]].drop_duplicates().set_index("company_id")["company_name"].to_dict()
+
+def get_transactions(company_id):
+    trans_url = f'https://circuit-trial.stg.rd.ds.sansan.com/api/contacts/owner_companies/{company_id}?offset=0&limit=1000'
+    trans_r = requests.get(trans_url)
+    trans_df = pd.DataFrame(trans_r.json())
+    return format_trans_table(trans_df, mst_df)
+
+
 # タイトル
-st.title("サンプルアプリ")
+st.title("人事評価")
 
-path = Path(__file__).parent / "dummy_data.csv"
-df_dummy = pd.read_csv(path.as_uri(), dtype=str)
+company_name = st.selectbox("会社名", list(company_dict.values()))
+company_id = [k for k, v in company_dict.items() if v == company_name][0]
+st.write(f"選択中の会社: {company_name}, id: {company_id}")
 
-st.dataframe(df_dummy)
+submit = st.button("取引履歴を取得")
+if submit:
+    if company_id:
+        try:
+            trans_df = get_transactions(company_id)
+            st.table(trans_df)
+        except Exception as e:
+            st.write(f"エラーが発生しました: {e}")
+
+# if submit:
+#     if company_id is not None and trans_df is not None:
+#         owner = trans_df["owner_user_id"]
+#         user = trans_df["user_id"]
+
+#         owner_job = mst_df[mst_df["user_id"] == owner].position.values[0]
+#         user_job = mst_df[mst_df["user_id"] == user].position.values[0]
+#         st.write(compare_positions(owner_job, user_job))
---
+++ app/module.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+positions = {
+    7: ["部長", "技術部長", "営業部長"],
+    6: ["次長", "技術次長", "営業次長", "課長", "技術課長", "営業課長"],
+    5: ["係長", "技術係長", "営業係長", "主任", "技術主任", "営業主任"],
+    4: ["プロダクトマネージャー", "プロジェクトマネージャー", "アカウントマネージャー", "カスタマーサポートマネージャー", "アシスタントマネージャー"],
+    3: ["シニアアナリスト", "リードエンジニア"],
+    2: ["一般社員", "新入社員"],
+    1: ["アナリスト", "ジュニアアナリスト", "ソフトウェアエンジニア", "システムエンジニア", "データサイエンティスト"]
+}
+
+# 役職名からランクを取得する辞書
+position_to_rank = {position: rank for rank, positions in positions.items() for position in positions}
+
+def get_position_rank(position):
+    return position_to_rank.get(position, None)
+
+def compare_positions(position1, position2):
+    rank1 = get_position_rank(position1)
+    rank2 = get_position_rank(position2)
+    
+    if rank1 is None or rank2 is None:
+        raise ValueError("One or both positions are invalid")
+    
+    return rank1 - rank2
+
+def format_trans_table(trans_df, mst_df):
+    # trans_dfの_owner_user_id列をmst_dfの名前に変換
+    trans_df["score"] = trans_df.apply(lambda x: compare_positions(mst_df[mst_df["user_id"] == x["owner_user_id"]].position.values[0], mst_df[mst_df["user_id"] == x["user_id"]].position.values[0]), axis=1)
+
+    trans_df["owner_user_id"] = trans_df["owner_user_id"].apply(lambda x: mst_df[mst_df["user_id"] == x].iloc[0]["full_name"])
+    trans_df["user_id"] = trans_df["user_id"].apply(lambda x: mst_df[mst_df["user_id"] == x].iloc[0]["full_name"])
+
+    return trans_df
+
+def apply_score(r):
+    pass
