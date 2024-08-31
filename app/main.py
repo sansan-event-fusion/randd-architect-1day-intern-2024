112eaf48dc003dbec9afebb155653244221063b1
kip-i <kppisj@gmail.com>
2024-08-31 15:49:01 +0900

    add: 同じ会社の人が持ってる情報を表示

--- app/main.py
+++ app/main.py
@@ -1,12 +1,46 @@
-from pathlib import Path
-
+import requests
 import pandas as pd
+
 import streamlit as st
 
-# タイトル
-st.title("サンプルアプリ")
 
-path = Path(__file__).parent / "dummy_data.csv"
-df_dummy = pd.read_csv(path.as_uri(), dtype=str)
+st.title('名刺交換表示')
+url = 'https://circuit-trial.stg.rd.ds.sansan.com/api/'
 
-st.dataframe(df_dummy)
+# session_stateにcardsがない場合はcardsを追加
+if 'cards' not in st.session_state:
+    st_cards = requests.get(url + 'cards/?offset=0&limit=2000')
+    st.session_state.cards = st_cards.json()
+    st_cards_json = {}
+    for card in st_cards.json():
+        st_cards_json[card['user_id']] = card
+    st.session_state.cards_json = st_cards_json
+    st.session_state.cards_df = pd.DataFrame(st_cards.json())
+if 'contacts' not in st.session_state:
+    st_contacts = requests.get(url + 'contacts/?offset=0&limit=99439')
+    st.session_state.contacts = st_contacts.json()
+    st.session_state.contacts_df = pd.DataFrame(st_contacts.json())
+
+cards = st.session_state.cards
+contacts = st.session_state.contacts
+cards_json = st.session_state.cards_json
+cards_df = st.session_state.cards_df
+contacts_df = st.session_state.contacts_df
+
+with st.expander('ログイン情報を入力して下さい'):
+    user_id = st.text_input('ユーザーID')
+    phone_number = st.text_input('電話番号')
+
+if user_id in cards_json and user_id != '' and phone_number!= '' and phone_number == cards_json[user_id]['phone_number']:
+    # 会社内の名刺を表示 company_idが一致するものを表示
+    same_company_cards = cards_df[cards_df['company_id'] == cards_json[user_id]['company_id']]
+    # 会社内の所有する名刺を表示
+    same_company_contacts = contacts_df[contacts_df['owner_user_id'].isin(same_company_cards['user_id'])]
+    for i, row in same_company_contacts.iterrows():
+        user = cards_json[row['user_id']]
+        with st.popover(f'{user["full_name"]}様 [{user["company_name"]}]'):
+            st.write(f'{user["company_name"]}　{user["position"]}')
+            st.write(f'住所:{user["address"]}, 　電話番号:{user["phone_number"]}')
+            st.write(f'{cards_json[row["owner_user_id"]]["full_name"]}が取得しました。')
+elif user_id != '' and phone_number != '':
+    st.write('ログイン情報が間違っています')
