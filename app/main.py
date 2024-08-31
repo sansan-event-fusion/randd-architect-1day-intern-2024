22849c207e3f9485e1ee48c2908ce2c24f825664
Fujii Rise <pi@cheru.jp>
2024-08-31 16:07:48 +0900

    ruff add

--- app/main.py
+++ app/main.py
@@ -11,11 +11,14 @@ API_cards_URL = "https://circuit-trial.stg.rd.ds.sansan.com/api/cards/?offset=0&
 # API_contacts
 API_contacts_URL = "https://circuit-trial.stg.rd.ds.sansan.com/api/contacts/?offset=0&limit=100"
 
-#API_contacts_users
-API_contacts_users_URL = "https://circuit-trial.stg.rd.ds.sansan.com/api/contacts/owner_users/4578252120?offset=0&limit=100"
+# API_contacts_users
+API_contacts_users_URL = (
+    "https://circuit-trial.stg.rd.ds.sansan.com/api/contacts/owner_users/4578252120?offset=0&limit=100"
+)
 
 # タイトル
 st.title("Reconnect Card")
+
 
 # APIから名刺情報を取得
 @st.cache_data
@@ -28,6 +31,7 @@ def fetch_business_cards():
         st.error(f"名刺情報の取得に失敗しました: {e}")
         return pd.DataFrame()
 
+
 # APIから名刺交換履歴を取得
 @st.cache_data
 def fetch_business_contacts():
@@ -38,6 +42,7 @@ def fetch_business_contacts():
     except requests.exceptions.RequestException as e:
         st.error(f"名刺交換履歴の取得に失敗しました: {e}")
         return pd.DataFrame()
+
 
 # APIから名刺交換日時を取得
 @st.cache_data
@@ -50,9 +55,9 @@ def fetch_exchange_history_date():
         st.error(f"名刺交換履歴の取得に失敗しました: {e}")
         return pd.DataFrame()
 
-    
+
 # 名刺情報の表示
-st.subheader('名刺情報')
+st.subheader("名刺情報")
 cards_df = fetch_business_cards()
 if not cards_df.empty:
     st.dataframe(cards_df)
@@ -60,7 +65,7 @@ else:
     st.write("名刺情報がありません。")
 
 # 名刺交換履歴の表示
-st.subheader('名刺交換履歴')
+st.subheader("名刺交換履歴")
 history_df = fetch_business_contacts()
 if not history_df.empty:
     st.dataframe(history_df)
@@ -68,7 +73,7 @@ else:
     st.write("名刺交換履歴がありません。")
 
 # 指定したユーザーの名刺交換情報の表示
-st.subheader('指定したユーザーの名刺交換情報')
+st.subheader("指定したユーザーの名刺交換情報")
 history_df = fetch_exchange_history_date()
 if not history_df.empty:
     st.dataframe(history_df)
